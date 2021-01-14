4d82871b5b99af686d55d9b45e68edbc82d2ae27
Siyu <jiansiyu@gmail.com>
2021-01-14 17:43:49 -0500

    jobsubmitter works

--- scripts/JobSubmit/gemgo.py
+++ scripts/JobSubmit/gemgo.py
@@ -9,6 +9,7 @@ import os
 import json
 import sys
 import random
+import csv
 
 class runErrorList:
     FileNotFoundList=[]
@@ -17,22 +18,76 @@ class runErrorList:
         pass
     def MissingRawFile(self,runID):
         self.FileNotFoundList.append(runID)
-
     def Save(self):
         pass
 
 runErrorList = runErrorList()
 
 class gemJobs:
-    Type = ""
+    runType = ""
     runID = 0
-    filePath = ""
-    fileNameTemplate = ""
-    ThreadID = 0
+    SavePedestal = ""
+    LoadPedestal = ""
     Mapping=""
+    defaultSavePath=""
+    defaultRawPath =""
+    defaultPedestalPath = ""
+    HitRootFile =""
+    rawFileName=""
+    nfiles = 3
     FLag = False
-    def __init__(self,runType="",runID=0,Mapping="",SavePedestal="",Pedestal="",nevent=0,HitRootFile ="",Nfile=0):
-        pass
+    rawDataStri=""
+    def __init__(self,runType="",runID=0,Mapping="",SavePedestal="",LoadPedestal="",nevent=0,HitRootFile ="",Nfile=0):
+        self.runID = runID
+        self.runType = runType
+        self.Mapping = Mapping
+        self.SavePedestal = SavePedestal
+        self.LoadPedestal = LoadPedestal
+        self.HitRootFile = HitRootFile
+    def Validate(self):
+        if self.runID == 0 :
+            return False
+        if not self.Mapping :
+            return  False
+        if self.runType == "GetRootFile" and not self.LoadPedestal:
+            return False
+        return  True
+
+    def getrunConfig(self,JobSavePath=""):
+        if not self.Validate():
+            return False
+        if not self.SavePedestal:
+            fname = "pedestal_LHRS_{runID}.root".format(runID = self.runID)
+            if self.runID > 20000:
+                fname = "pedestal_RHRS_{runID}.root".format(runID =self.runID)
+            self.SavePedestal = os.path.join(self.defaultPedestalPath,fname)
+
+        if not self.HitRootFile:
+            rawfname = "prexLHRS_{}.root".format(self.runID)
+            if self.runID > 20000:
+                rawfname = "prexRHRS_{}.root".format(self.runID)
+            self.HitRootFile = os.path.join(self.defaultSavePath,rawfname)
+
+        if not self.rawDataStri:
+            rawfname = "prexLHRS_{}.dat".format(self.runID)
+            if self.runID > 20000:
+                rawfname = "prexRHRS_{}.dat".format(self.runID)
+            self.rawDataStri = os.path.join(self.defaultRawPath, rawfname) + ", 0, {}".format(self.nfiles)
+
+        if self.runType == "CalPedestal" and not self.LoadPedestal:
+            self.LoadPedestal = self.SavePedestal
+
+        configJob = runConfigGenerator(runType=self.runType,Mapping=self.Mapping,SavedPed=self.SavePedestal,LoadPed=self.LoadPedestal,\
+                                       Nevent=self.nfiles*1000000,HitRootFile=self.HitRootFile,NFIle=self.nfiles,InputString=self.rawDataStri)
+        configJob.SaveConfig(fname=JobSavePath)
+    def getRawfilename(self):
+        if not self.rawFileName:
+            filename = "prexLHRS_{}.dat".format(self.runID)
+            if self.runID > 20000:
+                filename = "prexRHRS_{}.dat".format(self.runID)
+
+            self.rawFileName = os.path.join(self.defaultRawPath,filename)
+        return  self.rawFileName
 
     def getRunType(self):
         pass
@@ -53,7 +108,7 @@ class runConfigGenerator:
                "#---------------------------------------------------------------\n" \
                "#GEM analysis configure file\n" \
                "#---------------------------------------------------------------\n" \
-               "# runType\n" \
+               "# runType,0 ,0\n" \
                "#---------------------------------------------------------------\n" \
                "RUNTYPE: {runType}\n" \
                "#---------------------------------------------------------------\n" \
@@ -132,6 +187,9 @@ class JobSubmitter:
     replayMappingFile = ""
     replayNevent = 0
     JobConfigSavePath = ""
+    defaultRawPath = ""
+    defaultPedestalPath =""
+    defaultSavePath = ""
     def __init__(self, server="local"):
         self.server = server
         if not server:
@@ -161,14 +219,55 @@ class JobSubmitter:
                 self.replayMappingFile = data["MAPPING"]
                 self.replayNevent = int(data["NEVENT"])
                 self.JobConfigSavePath = data["ConfigSavePath"]
+                self.defaultRawPath = data["defaultRawPath"]
+                self.defaultPedestalPath = data["defaultPedestalPath"]
+                self.defaultSavePath = data["defaultSavePath"]
         except:
             logging.warning("Can not decode json file {}, Use with Caution!!".format(Config))
+
+    def SearchRawRange(self,fpath=""):
+        if not fpath:
+            return None
+        if not self.isfileexist(filename="{}.{}".format(fpath,0)):
+            return None
+        for i in range(0,100):
+            if not self.isfileexist(filename="{}.{}".format(fpath,i)):
+                break
+
+        return [0,i]
+
+    def ParserCSV(self,runfname=""):
+        if not runfname or not self.isfileexist(runfname):
+            logging.error("Can not find csv run List {}".format(runfname))
+            return
+        runList = []
+        try:
+            with open(runfname,newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    runID = int(row["runID"])
+                    runType = row["runType"]
+                    Mapping = row["Mapping"]
+                    SavePedestal = row["savepedestal"]
+                    LoadPedestal = row["pedestal"]
+                    if "pedestal" in runType.lower():
+                        runType = "CalPedestal"
+                    elif "root" in runType.lower():
+                        runType = "GetRootFile"
+                    else:
+                        logging.error("Unsupport runType {}".format(runType))
+                    runList.append(gemJobs(runType=runType,runID=runID,Mapping=Mapping,SavePedestal=SavePedestal,LoadPedestal=LoadPedestal))
+        except:
+            logging.error("Error when parser CSV {}".format(runfname))
+        return  runList
+
+    def submitJob(self):
+        pass
 
 class iFarmSubmitter(JobSubmitter):
 
     def __init__(self):
         super().__init__(server='ifarm')
-
 
 class localSubmitter(JobSubmitter):
     JobArray=[]
@@ -176,27 +275,27 @@ class localSubmitter(JobSubmitter):
 
     def __init__(self):
         super().__init__(server='local')
+        print(self.defaultRawPath)
+        print(self.defaultSavePath)
 
-    def replaySingleRun(self,runID,runType="",nevent=1000000):
-        fileNameTemplate = "prexLHRS_{runID}.dat"
-        if runID > 20000:
-            fileNameTemplate = "prexRHRS_{runID}.dat"
-        fileNameTemplate = fileNameTemplate.format(runID = runID)
-
-        if "pedestal" in runType.lower():
-            runType = "CalPedestal"
-        elif "root" in runType.lower():
-            runType = "GetRootFile"
+    def replaySingleRun(self,job=gemJobs()):
+        if job.Validate():
+            job.getrunConfig(JobSavePath=os.path.join(self.JobConfigSavePath,"runConfig_{}.cfg".format(job.runID)))
         else:
-            logging.error("Unsupport runType {}".format(runType))
-            return False
+            logging.warning("Error run {}".format(job.runID))
 
-        # create the configure file
-        Mapping = ""
-        
+    def replayCSV(self,fname="replayList.csv"):
+        for job in self.ParserCSV(runfname=fname):
 
-    def _replayCSV(self):
-        pass
+            if not job.Mapping:
+                job.Mapping = self.replayMappingFile
+
+            job.defaultRawPath = self.defaultRawPath
+            job.defaultSavePath = self.defaultSavePath
+            job.defaultPedestalPath = self.defaultPedestalPath
+            if self.SearchRawRange(job.getRawfilename()):
+                job.nfiles = self.SearchRawRange(job.getRawfilename())[1]
+            self.replaySingleRun(job=job)
 
     def replay(self,runID=-999,runType ="",csvList=''):
         if runID != -999:
@@ -204,7 +303,6 @@ class localSubmitter(JobSubmitter):
 
 class TemplateDecoder:
     pass
-
 
 class tester:
     def __init__(self):
@@ -214,12 +312,21 @@ class tester:
         a = runConfigGenerator(runType="CalPedestal", Mapping="text.mapping", SavedPed="save.root", LoadPed="pesd.root",
                                Nevent=11000, HitRootFile="asasa.root", NFIle=2, InputString="sasasa.dat, 1 ,2")
         a.SaveConfig("text.cfg")
-
+    def jobsubmitter(self):
+        a = JobSubmitter(        )
+        a.ParserCSV(runfname="replayList.csv")
 
 if __name__ == '__main__':
-    a = tester()
-    a.runConfigTester()
-    print(os.path.dirname(os.path.realpath(__file__)))
+    # if len(sys.argv) >1:
+    #     for item in sys.argv:
+    #         if item.endswith(".csv"):
+    #             if "ifarm" not  in os.uname().nodename:
+    runner = localSubmitter()
+    runner.replayCSV()
+
+    # a = tester()
+    # a.jobsubmitter()
+    # # print(os.path.dirname(os.path.realpath(__file__)))
     # run locally
     # if "siyu" not in os.uname().nodename:
     #     a=JobSubmitter()
